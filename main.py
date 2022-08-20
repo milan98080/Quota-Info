2fdc260c792f749a20db7a6592706b36c2e9be83
Milan Pokhrel <54058328+milan98080@users.noreply.github.com>
2022-08-20 17:13:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import json
 from threading import Timer
 
 cookies = {
-    'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d': '776653%7CZiGzgUaFnjy6afetK7HyZ4F4BGog2BRuioi67E3L5rEBP1Fzc68do2jNukIb%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24aEwvY3N5S0wvanVIaFJkQw%24NmYohpLyTc9cUSKbSgIhmvyXMeYRqSe52eqmxNVqYz4',
-    'XSRF-TOKEN': 'eyJpdiI6IksxWk1MYnRIMGpuSlg0c2E1VGRiQWc9PSIsInZhbHVlIjoiOTY5OXJVK3U2eE5wUFM2QzM2T0ZSd0xMUEtFTzBWWGNIS20yZVlxVUIwYWdUZVJvTzJYbDg2WEpNUXljR1RUd1lzZ3gwWFhmVURkUWwxSWNzdDZnRXlBazNkU0V0QXgzL3NESC95SWtmMFlQQXVqc3BuMFI4dlRGU2hMc253OEgiLCJtYWMiOiIyNzM1ODA3MGY0NmVjNWFkYjY3Y2RjZTc4ODkwNTYxZmVlZTI4NzI4NjczYThiMTBlOTU1NWQxZTExOTM3MGFkIiwidGFnIjoiIn0%3D',
-    'odl_session': 'upRKaZvmmpJViwLW7LW9WafJatmpt28a3a6Zn6cw',
+    'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d': '694691%7CGdkDFZaSj2zNcW1zMMFERRPRBYxi5vLliweNp6fhiA7E0CImxddlKQDHDjWU%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24T1laYXEyNVphZUszaDJBWg%241e%2FFwRiaaYOGIxxNvJfYrWlO0rVRfvocmN3vKrIhQNs',
+    'XSRF-TOKEN': 'eyJpdiI6InhqcXpqTmh6VlE3cGVQUWZGNkpHcFE9PSIsInZhbHVlIjoiNlFLMVVQdUNvMWtlSjhKOFJ3K3lwT3AxdU5oL3d5OXh6ZDNrTjVGMWpXK2VnK2RmVWhoRHFXRkcra1NsTnpVcEQ5eUpGZnNKQ1BSWkFTWGR3N3kvSEk2Z0psTzl5b2MydjN5Yi9qZlVnK3BqejREMFZyZ1pVZk1NNjEzc2d2M2EiLCJtYWMiOiJkYjc4NWQxYWE1YjliOTdmMmQ2ZWI4MDQyMWQxNjUyZGE4YzNkYjRkYWQ0MzU4MDliNDM3YzFlZWQ3ZTVkYWVmIiwidGFnIjoiIn0%3D',
+    'odl_session': 'adVyA6NsfLJOkI8vWJpN2X2n8NGsiK9JqhiAIs49',
 }
 
 headers = {
@@ -15,12 +15,12 @@ headers = {
     # 'Accept-Encoding': 'gzip, deflate, br',
     # Already added when you pass json=
     # 'Content-Type': 'application/json',
-    'X-XSRF-TOKEN': 'eyJpdiI6IksxWk1MYnRIMGpuSlg0c2E1VGRiQWc9PSIsInZhbHVlIjoiOTY5OXJVK3U2eE5wUFM2QzM2T0ZSd0xMUEtFTzBWWGNIS20yZVlxVUIwYWdUZVJvTzJYbDg2WEpNUXljR1RUd1lzZ3gwWFhmVURkUWwxSWNzdDZnRXlBazNkU0V0QXgzL3NESC95SWtmMFlQQXVqc3BuMFI4dlRGU2hMc253OEgiLCJtYWMiOiIyNzM1ODA3MGY0NmVjNWFkYjY3Y2RjZTc4ODkwNTYxZmVlZTI4NzI4NjczYThiMTBlOTU1NWQxZTExOTM3MGFkIiwidGFnIjoiIn0=',
+    'X-XSRF-TOKEN': 'eyJpdiI6InhqcXpqTmh6VlE3cGVQUWZGNkpHcFE9PSIsInZhbHVlIjoiNlFLMVVQdUNvMWtlSjhKOFJ3K3lwT3AxdU5oL3d5OXh6ZDNrTjVGMWpXK2VnK2RmVWhoRHFXRkcra1NsTnpVcEQ5eUpGZnNKQ1BSWkFTWGR3N3kvSEk2Z0psTzl5b2MydjN5Yi9qZlVnK3BqejREMFZyZ1pVZk1NNjEzc2d2M2EiLCJtYWMiOiJkYjc4NWQxYWE1YjliOTdmMmQ2ZWI4MDQyMWQxNjUyZGE4YzNkYjRkYWQ0MzU4MDliNDM3YzFlZWQ3ZTVkYWVmIiwidGFnIjoiIn0=',
     'Origin': 'https://applydl.dotm.gov.np',
     'Connection': 'keep-alive',
-    'Referer': 'https://applydl.dotm.gov.np/license/apply',
+    'Referer': 'https://applydl.dotm.gov.np/license/category',
     # Requests sorts cookies= alphabetically
-    # 'Cookie': 'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d=776653%7CZiGzgUaFnjy6afetK7HyZ4F4BGog2BRuioi67E3L5rEBP1Fzc68do2jNukIb%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24aEwvY3N5S0wvanVIaFJkQw%24NmYohpLyTc9cUSKbSgIhmvyXMeYRqSe52eqmxNVqYz4; XSRF-TOKEN=eyJpdiI6IksxWk1MYnRIMGpuSlg0c2E1VGRiQWc9PSIsInZhbHVlIjoiOTY5OXJVK3U2eE5wUFM2QzM2T0ZSd0xMUEtFTzBWWGNIS20yZVlxVUIwYWdUZVJvTzJYbDg2WEpNUXljR1RUd1lzZ3gwWFhmVURkUWwxSWNzdDZnRXlBazNkU0V0QXgzL3NESC95SWtmMFlQQXVqc3BuMFI4dlRGU2hMc253OEgiLCJtYWMiOiIyNzM1ODA3MGY0NmVjNWFkYjY3Y2RjZTc4ODkwNTYxZmVlZTI4NzI4NjczYThiMTBlOTU1NWQxZTExOTM3MGFkIiwidGFnIjoiIn0%3D; odl_session=upRKaZvmmpJViwLW7LW9WafJatmpt28a3a6Zn6cw',
+    # 'Cookie': 'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d=694691%7CGdkDFZaSj2zNcW1zMMFERRPRBYxi5vLliweNp6fhiA7E0CImxddlKQDHDjWU%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24T1laYXEyNVphZUszaDJBWg%241e%2FFwRiaaYOGIxxNvJfYrWlO0rVRfvocmN3vKrIhQNs; XSRF-TOKEN=eyJpdiI6InhqcXpqTmh6VlE3cGVQUWZGNkpHcFE9PSIsInZhbHVlIjoiNlFLMVVQdUNvMWtlSjhKOFJ3K3lwT3AxdU5oL3d5OXh6ZDNrTjVGMWpXK2VnK2RmVWhoRHFXRkcra1NsTnpVcEQ5eUpGZnNKQ1BSWkFTWGR3N3kvSEk2Z0psTzl5b2MydjN5Yi9qZlVnK3BqejREMFZyZ1pVZk1NNjEzc2d2M2EiLCJtYWMiOiJkYjc4NWQxYWE1YjliOTdmMmQ2ZWI4MDQyMWQxNjUyZGE4YzNkYjRkYWQ0MzU4MDliNDM3YzFlZWQ3ZTVkYWVmIiwidGFnIjoiIn0%3D; odl_session=adVyA6NsfLJOkI8vWJpN2X2n8NGsiK9JqhiAIs49',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -54,7 +54,7 @@ def run():
 
 
 def startt():
-    Timer(30, startt).start()
+    Timer(5, startt).start()
     run()
 
 
