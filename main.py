001ef7615027c75e3ce3c67c0107bc2f712d7688
Milan Pokhrel <54058328+milan98080@users.noreply.github.com>
2022-08-20 17:26:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ def run():
         new = data['quotas']
         print(cate)
         for every in data['quotas']:
+            parsed = new[every]
             if parsed['ad'] == '2022-09-05':
-                parsed = new[every]
                 print(parsed['ad'] + '  AVAILABLE  ' + str(parsed['available']))
         print('  ')
 
