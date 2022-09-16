afd9cc3751699373463f0bc2923434e450ddd280
Milan Pokhrel <54058328+milan98080@users.noreply.github.com>
2022-09-16 19:38:18 +0545

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 import requests
 
 cookies = {
-    'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d': '862186%7Cm5OJHdNZeknnaFFivrejU5WGeOmewZvR3jo1S9PMWRIdfJ40LX3Gwuv4MXxs%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24dzE2Zm80b1lYYTQ0bDdIOA%24CDm8DJxmYTsyn1IB9eHdIPArpmhLwCUXwpnu4zW7DoY',
-    'XSRF-TOKEN': 'eyJpdiI6IkVlN3lQUTBQUkZjQmh3NUZ6N2taZVE9PSIsInZhbHVlIjoiS1N3ZzRVL016QW1NL0h5dyt0eUc1SmRabmIzOC9WekltMWtBY0RNNlpxdVV6VDVleFJ5MWJid29IWFlnT0p6cmwrYjArLzIvSVBHWisxeEpUQU5TRXpqb25kZ1JmS001eTcybitwcWIyMUJ0ZHFlSktPM3g0WFVJcWcrQy9xcFQiLCJtYWMiOiI3MjliODI0OTAyNGE2ZGM1YmMzYTI0NTZkNzQ3ODUwMGE5YzVhMWY0MWJmYWIyZjJkNDRjYjQwYThhNjQ4YzliIiwidGFnIjoiIn0%3D',
-    'odl_session': 'CCrZ8wkKPtb6hC2S6Ha5F1SVn3e8pTCPBDVqUcw7',
+    'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d': '869111%7CSIlD7wOH5YNpUJQ25PN24y9F6KUHD1e6IDscJjb8EB42UwG5gGMf7BMdBZSt%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24OGQ0Z0hRMTdGNC52Sk5TUQ%24P%2BcUsvppt318xbw4aGQW2KZCGXUDNlOiCGNli%2FXpnYk',
+    'XSRF-TOKEN': 'eyJpdiI6IkRJc0tEVVlBU0FWRzJreWY3aW5MNVE9PSIsInZhbHVlIjoiKzc3U3JQWFhMKzFmRU1iR1UwSFlhN3IzY1oycGxYeGlLK0lCdUpaOXNkbDBoK3l2TEgxTTJzbGVCeUtkc1NJWjl1T01nSm96YmYxaS9wd3FlVEFsSWVIdjZsQ2xGMS9OR3F6NDhkWVFZdER0K3NCZk1aNUlBNGc5eTZQQWh2cy8iLCJtYWMiOiI4NWJhNWIzODliZDRhYTY3NThhYzllY2NkMzY5M2EzNjE1NmE0ZGY4MGIzOTRmZjdjYzg4YWE1YmEyZjg4ZjBiIiwidGFnIjoiIn0%3D',
+    'odl_session': 'kjgsDZtq06XWqOfJGvUvYGwMenGtT30DqYDEmEFy',
 }
 
 headers = {
@@ -15,31 +15,24 @@ headers = {
     # 'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
     'X-Inertia': 'true',
-    'X-Inertia-Version': '16361033bc8dabc69ba761dce03df65e',
-    'X-XSRF-TOKEN': 'eyJpdiI6IkVlN3lQUTBQUkZjQmh3NUZ6N2taZVE9PSIsInZhbHVlIjoiS1N3ZzRVL016QW1NL0h5dyt0eUc1SmRabmIzOC9WekltMWtBY0RNNlpxdVV6VDVleFJ5MWJid29IWFlnT0p6cmwrYjArLzIvSVBHWisxeEpUQU5TRXpqb25kZ1JmS001eTcybitwcWIyMUJ0ZHFlSktPM3g0WFVJcWcrQy9xcFQiLCJtYWMiOiI3MjliODI0OTAyNGE2ZGM1YmMzYTI0NTZkNzQ3ODUwMGE5YzVhMWY0MWJmYWIyZjJkNDRjYjQwYThhNjQ4YzliIiwidGFnIjoiIn0=',
+    'X-Inertia-Version': 'c7f6cd80bbaaa326d1c96774af7f4434',
+    'X-XSRF-TOKEN': 'eyJpdiI6IkRJc0tEVVlBU0FWRzJreWY3aW5MNVE9PSIsInZhbHVlIjoiKzc3U3JQWFhMKzFmRU1iR1UwSFlhN3IzY1oycGxYeGlLK0lCdUpaOXNkbDBoK3l2TEgxTTJzbGVCeUtkc1NJWjl1T01nSm96YmYxaS9wd3FlVEFsSWVIdjZsQ2xGMS9OR3F6NDhkWVFZdER0K3NCZk1aNUlBNGc5eTZQQWh2cy8iLCJtYWMiOiI4NWJhNWIzODliZDRhYTY3NThhYzllY2NkMzY5M2EzNjE1NmE0ZGY4MGIzOTRmZjdjYzg4YWE1YmEyZjg4ZjBiIiwidGFnIjoiIn0=',
     'Origin': 'https://applydl.dotm.gov.np',
     'Connection': 'keep-alive',
-    'Referer': 'https://applydl.dotm.gov.np/license/apply/verification',
+    'Referer': 'https://applydl.dotm.gov.np/change/password/verification',
     # Requests sorts cookies= alphabetically
-    # 'Cookie': 'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d=862186%7Cm5OJHdNZeknnaFFivrejU5WGeOmewZvR3jo1S9PMWRIdfJ40LX3Gwuv4MXxs%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24dzE2Zm80b1lYYTQ0bDdIOA%24CDm8DJxmYTsyn1IB9eHdIPArpmhLwCUXwpnu4zW7DoY; XSRF-TOKEN=eyJpdiI6IkVlN3lQUTBQUkZjQmh3NUZ6N2taZVE9PSIsInZhbHVlIjoiS1N3ZzRVL016QW1NL0h5dyt0eUc1SmRabmIzOC9WekltMWtBY0RNNlpxdVV6VDVleFJ5MWJid29IWFlnT0p6cmwrYjArLzIvSVBHWisxeEpUQU5TRXpqb25kZ1JmS001eTcybitwcWIyMUJ0ZHFlSktPM3g0WFVJcWcrQy9xcFQiLCJtYWMiOiI3MjliODI0OTAyNGE2ZGM1YmMzYTI0NTZkNzQ3ODUwMGE5YzVhMWY0MWJmYWIyZjJkNDRjYjQwYThhNjQ4YzliIiwidGFnIjoiIn0%3D; odl_session=CCrZ8wkKPtb6hC2S6Ha5F1SVn3e8pTCPBDVqUcw7',
+    # 'Cookie': 'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d=869111%7CSIlD7wOH5YNpUJQ25PN24y9F6KUHD1e6IDscJjb8EB42UwG5gGMf7BMdBZSt%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24OGQ0Z0hRMTdGNC52Sk5TUQ%24P%2BcUsvppt318xbw4aGQW2KZCGXUDNlOiCGNli%2FXpnYk; XSRF-TOKEN=eyJpdiI6IkRJc0tEVVlBU0FWRzJreWY3aW5MNVE9PSIsInZhbHVlIjoiKzc3U3JQWFhMKzFmRU1iR1UwSFlhN3IzY1oycGxYeGlLK0lCdUpaOXNkbDBoK3l2TEgxTTJzbGVCeUtkc1NJWjl1T01nSm96YmYxaS9wd3FlVEFsSWVIdjZsQ2xGMS9OR3F6NDhkWVFZdER0K3NCZk1aNUlBNGc5eTZQQWh2cy8iLCJtYWMiOiI4NWJhNWIzODliZDRhYTY3NThhYzllY2NkMzY5M2EzNjE1NmE0ZGY4MGIzOTRmZjdjYzg4YWE1YmEyZjg4ZjBiIiwidGFnIjoiIn0%3D; odl_session=kjgsDZtq06XWqOfJGvUvYGwMenGtT30DqYDEmEFy',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
 }
 
 json_data = {
-    'category_id': 1,
-    'province_id': 4,
-    'office_id': 19,
-    'visit_date_bs': '2079-05-26',
-    'visit_date_ad': '2022-09-11',
-    'is_urgent': False,
-    'urgency_reason_id': '',
-    'documents': {},
-    'disclaimer': True,
+    'password': 'Pokhara@123',
+    'current_password': 'Pokhara@123',
+    'password_confirmation': 'Pokhara@123',
     'otp': '000000',
-    'verification': 'eyJpdiI6IjBUeWZZUzMxOXdLV21UQUJwVUpBYlE9PSIsInZhbHVlIjoiVjhqbXpreFFRei9nMXNCcnBjYlFvT3ByYTg2cTBoN3ladHJFdWdBQjhjND0iLCJtYWMiOiI2NWIxZDMzNTE0NWY4NjQyNmQwNDE1ZDU0MzlhZDA3ZjE5NWUzNzg3M2UxNDllN2U2ZGE2MTM4ZDQ4YTc3YzM4IiwidGFnIjoiIn0=',
+    'verification': 'eyJpdiI6IkdGSGNRU2t0L29TUW4vNkNDT0NIR0E9PSIsInZhbHVlIjoib0s4NzNITmx5anMxUE54bS9vRnl1SUxnd0gza1hVWWFvSVhqMi9sQTc3TT0iLCJtYWMiOiIyNmU1YTIwMGY4ZTgyMzk5M2FhNTUwMGRiNzdlMDEzNTNjYTg2ZTk0YzBiMGE2MzE1NzQ4NTE1OTA3NWEzYWMxIiwidGFnIjoiIn0=',
 }
 
-response = requests.post('https://applydl.dotm.gov.np/license/apply', cookies=cookies, headers=headers, json=json_data)
-print(response)
+response = requests.post('https://applydl.dotm.gov.np/change/password', cookies=cookies, headers=headers, json=json_data)
