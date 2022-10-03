2cb0d1b8e4707ab4b4d34f34cd6e12c38cef606a
Milan Pokhrel <54058328+milan98080@users.noreply.github.com>
2022-10-03 08:42:35 +0545

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import json
 from threading import Timer
 
 cookies = {
-    'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d': '968507%7Cl8wSiDJ8zv8v4x9RHzbWRW66wxoN9mmw0NofCjwmRLR6iGRhJiLdxMsRADmn%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24bVd4akptNTIuY2xua3pDZQ%24FYu70tCWa%2FRgTkV8aUT%2BypX1akzTskzELKjwW7WAMvI',
-    'odl_session': 'O9UA2hgWNkWyKZ4BdzlrP5g1QeHqsseGpWnWO5mg',
-    'XSRF-TOKEN': 'eyJpdiI6InU2Y1BUaTdlaEI5VGdyZ2ZLTzRxc2c9PSIsInZhbHVlIjoidDdEWGpxdlhrRTNWY0ZJSS8wVk1hbUkvNDRPY1NWazB5cUNBeldxU2N1RTBVTnBlSi9hbWtiOGpLa3B4RVdoWlI0UXoxMGJaZEM0cmZ5eFNiaGZsc0VjUTY2N1U1cmJ5Qk9PKytyL1pSNmJFUEg0d2VOZm9YZ3MyQWxUaE1NMngiLCJtYWMiOiI3MTVkNzg2MWIxMDFmNTZkYmFkYzAwMjA3ZjU5NDFjNTZlMjU3NmU4MzBjMzMyNjBkM2ZiZDcxZWJlMjZkNDhiIiwidGFnIjoiIn0%3D',
+    'odl_session': 'i94oucXMXa2sANNF5wMnQ2HKfIiBQZ2FLiH2tMgA',
+    'XSRF-TOKEN': 'eyJpdiI6InhOMCtheGE0VmFGNkUra2J5NnFOM1E9PSIsInZhbHVlIjoiQm5GMndjU3Y5d0daem0zQnZicXhTTklqbU5JOXF6enpHZUlaRHpINHhod0ZXUmkxUEcyZFpSYXM4QTVrMWlzeFlLRGh1NkRQMTVONHphWlBDOGd5c3dqSHc2WFl3VG5wZm5VTjQzSWhBVGhNVG53T284aEVzZGd0MDFWbW1yeUoiLCJtYWMiOiI1MGI5NDU0M2MxMTliNTJkMzZjODllNmM5ODU4NzRiYjY4OTJlY2Y0NmM3OTM2NjRmZDEwMmFmNzAwMzQ3OTYyIiwidGFnIjoiIn0%3D',
 }
 
 headers = {
@@ -14,8 +13,8 @@ headers = {
     'Connection': 'keep-alive',
     'Content-Type': 'application/json',
     # Requests sorts cookies= alphabetically
-    # 'Cookie': 'remember_applicants_59ba36addc2b2f9401580f014c7f58ea4e30989d=968507%7Cl8wSiDJ8zv8v4x9RHzbWRW66wxoN9mmw0NofCjwmRLR6iGRhJiLdxMsRADmn%7C%24argon2id%24v%3D19%24m%3D65536%2Ct%3D4%2Cp%3D1%24bVd4akptNTIuY2xua3pDZQ%24FYu70tCWa%2FRgTkV8aUT%2BypX1akzTskzELKjwW7WAMvI; odl_session=O9UA2hgWNkWyKZ4BdzlrP5g1QeHqsseGpWnWO5mg; XSRF-TOKEN=eyJpdiI6InU2Y1BUaTdlaEI5VGdyZ2ZLTzRxc2c9PSIsInZhbHVlIjoidDdEWGpxdlhrRTNWY0ZJSS8wVk1hbUkvNDRPY1NWazB5cUNBeldxU2N1RTBVTnBlSi9hbWtiOGpLa3B4RVdoWlI0UXoxMGJaZEM0cmZ5eFNiaGZsc0VjUTY2N1U1cmJ5Qk9PKytyL1pSNmJFUEg0d2VOZm9YZ3MyQWxUaE1NMngiLCJtYWMiOiI3MTVkNzg2MWIxMDFmNTZkYmFkYzAwMjA3ZjU5NDFjNTZlMjU3NmU4MzBjMzMyNjBkM2ZiZDcxZWJlMjZkNDhiIiwidGFnIjoiIn0%3D',
-    'Referer': 'https://applydl.dotm.gov.np/licence/category',
+    # 'Cookie': 'odl_session=i94oucXMXa2sANNF5wMnQ2HKfIiBQZ2FLiH2tMgA; XSRF-TOKEN=eyJpdiI6InhOMCtheGE0VmFGNkUra2J5NnFOM1E9PSIsInZhbHVlIjoiQm5GMndjU3Y5d0daem0zQnZicXhTTklqbU5JOXF6enpHZUlaRHpINHhod0ZXUmkxUEcyZFpSYXM4QTVrMWlzeFlLRGh1NkRQMTVONHphWlBDOGd5c3dqSHc2WFl3VG5wZm5VTjQzSWhBVGhNVG53T284aEVzZGd0MDFWbW1yeUoiLCJtYWMiOiI1MGI5NDU0M2MxMTliNTJkMzZjODllNmM5ODU4NzRiYjY4OTJlY2Y0NmM3OTM2NjRmZDEwMmFmNzAwMzQ3OTYyIiwidGFnIjoiIn0%3D',
+    'Referer': 'https://applydl.dotm.gov.np/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -24,13 +23,13 @@ headers = {
     'X-Inertia-Version': 'abfe0d2fb15763f4d5db4390a711e362',
     'X-NewRelic-ID': 'VwMAWVNbCBAFVFVTDgEDX1U=',
     'X-Requested-With': 'XMLHttpRequest',
-    'X-XSRF-TOKEN': 'eyJpdiI6InU2Y1BUaTdlaEI5VGdyZ2ZLTzRxc2c9PSIsInZhbHVlIjoidDdEWGpxdlhrRTNWY0ZJSS8wVk1hbUkvNDRPY1NWazB5cUNBeldxU2N1RTBVTnBlSi9hbWtiOGpLa3B4RVdoWlI0UXoxMGJaZEM0cmZ5eFNiaGZsc0VjUTY2N1U1cmJ5Qk9PKytyL1pSNmJFUEg0d2VOZm9YZ3MyQWxUaE1NMngiLCJtYWMiOiI3MTVkNzg2MWIxMDFmNTZkYmFkYzAwMjA3ZjU5NDFjNTZlMjU3NmU4MzBjMzMyNjBkM2ZiZDcxZWJlMjZkNDhiIiwidGFnIjoiIn0=',
-    'newrelic': 'eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6IjM1Nzg1ODAiLCJhcCI6IjUwNDE5MDUwOCIsImlkIjoiY2RmOGJhNGQwZmYxNmU2MiIsInRyIjoiMTAwZGQwZmIxZDFjMTY2NjYzMmU2NWFlN2Q3NDVhMGMiLCJ0aSI6MTY2NDc2MTk3NDk1MX19',
+    'X-XSRF-TOKEN': 'eyJpdiI6InhOMCtheGE0VmFGNkUra2J5NnFOM1E9PSIsInZhbHVlIjoiQm5GMndjU3Y5d0daem0zQnZicXhTTklqbU5JOXF6enpHZUlaRHpINHhod0ZXUmkxUEcyZFpSYXM4QTVrMWlzeFlLRGh1NkRQMTVONHphWlBDOGd5c3dqSHc2WFl3VG5wZm5VTjQzSWhBVGhNVG53T284aEVzZGd0MDFWbW1yeUoiLCJtYWMiOiI1MGI5NDU0M2MxMTliNTJkMzZjODllNmM5ODU4NzRiYjY4OTJlY2Y0NmM3OTM2NjRmZDEwMmFmNzAwMzQ3OTYyIiwidGFnIjoiIn0=',
+    'newrelic': 'eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6IjM1Nzg1ODAiLCJhcCI6IjUwNDE5MDUwOCIsImlkIjoiMjQzMTg2NWI5YTBkNGJjMyIsInRyIjoiN2VkMDZiZDAyZmUyMjhmMThiOGMyMjYyMDgwNDhjY2IiLCJ0aSI6MTY2NDc2NTgyODI5NH19',
     'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
-    'traceparent': '00-100dd0fb1d1c1666632e65ae7d745a0c-cdf8ba4d0ff16e62-01',
-    'tracestate': '3578580@nr=0-1-3578580-504190508-cdf8ba4d0ff16e62----1664761974951',
+    'traceparent': '00-7ed06bd02fe228f18b8c226208048ccb-2431865b9a0d4bc3-01',
+    'tracestate': '3578580@nr=0-1-3578580-504190508-2431865b9a0d4bc3----1664765828294',
 }
 
 def run():
